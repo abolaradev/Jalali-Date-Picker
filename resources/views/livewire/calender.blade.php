dcb1b50340cd8b6ee43ec284958dc1582f3881c9
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-04 17:21:26 +0330

    feat: implement calendar core and initial UI

--- resources/views/livewire/calender.blade.php
+++ resources/views/livewire/calender.blade.php
@@ -1,14 +1,78 @@
 <?php
 
+use Abolaradev\JalaliDatePicker\Facades\JalaliDatePicker;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 new #[Layout('livewire-jalali-date-picker::layouts.app')] class extends Component
 {
+    public $grid;
+    public $today;
+    public $weekdays;
+
+    public function mount()
+    {
+        $this->grid=JalaliDatePicker::calenderGridLayout();
+        $this->today=JalaliDatePicker::dayOfMonth();
+        $this->weekdays=JalaliDatePicker::daysOfWeekAbbreviations();
+
     
+    }
 };
 ?>
 
-<div>
-    
+<div class=" h-screen flex items-center justify-center">
+     <div class="flex flex-col justify-around  text-center text-sm bg-fuchsia-50 border-stone-300 p-4 rounded-b-2xl gap-2" dir="rtl">
+
+           <div class="grid grid-cols-7 text-xs">
+
+               @foreach ($weekdays as $weekday)
+                    <span
+                         class="p-2 font-semibold bg-fuchsia-700 text-fuchsia-100"
+                         wire:key="weekday-{{ $loop->iteration }}">
+                         {{ $weekday }}
+                     </span>
+               @endforeach
+
+          </div>
+
+           <div class="grid grid-cols-7 gap-2">
+               @foreach ($grid['last-month'] as $day)
+                  <button
+                         type="button"
+                         class="h-9 rounded-lg px-3 bg-fuchsia-200 opacity-50 text-fuchsia-700"
+                         wire:key="last-month-day-{{ $day }}"
+                         @disabled(true)>
+                         {{ $day }}
+                    </button>
+               @endforeach
+
+               @foreach ($grid['current-month'] as $day)
+                    <button
+                         type="button"
+                         {{ $attributes->class([
+                              'h-9 rounded-lg px-3 cursor-pointer',
+                              'bg-fuchsia-700 text-fuchsia-100 font-semibold' => $day == $today,
+                              'bg-fuchsia-200 text-fuchsia-700' => $day != $today,
+                         ]) }}
+                            wire:key="current-month-day-{{ $day }}">
+                         {{ $day }}
+                    </button>
+               @endforeach
+
+                @foreach ($grid['next-month'] as $day)
+                  <button
+                         type="button"
+                         class="h-9 rounded-lg px-3 bg-fuchsia-200 opacity-50 text-fuchsia-700"
+                         wire:key="next-month-day-{{ $day }}"
+                         @disabled(true)>
+                         {{ $day }}
+                    </button>
+               @endforeach
+          </div>
+     </div>
 </div>
+
+@assets
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+@endassets
--- src/JalaliDatePicker.php
+++ src/JalaliDatePicker.php
@@ -2,4 +2,199 @@
 
 namespace Abolaradev\JalaliDatePicker;
 
-class JalaliDatePicker {}
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+use Morilog\Jalali\Jalalian;
+
+class JalaliDatePicker 
+{   
+
+    /**
+     * Obtaining the Jalali date
+     *
+     * @return Jalalian
+     */
+    public function now() :Jalalian
+    {
+        return Jalalian::now()->addMonths();
+    }
+    
+
+    /**
+     * Retrieving the names of the Jalali calendar months
+     *
+     * @return array
+     */
+    public function months() :array
+    {
+        $jalalian=$this->now()->getFirstDayOfYear();
+        $months=[];
+
+        for ($i=0; $i < 12; $i++) { 
+            $months[] = ($i == 0) ? $jalalian
+                                        : $jalalian->addMonths($i);
+        }
+        
+         $months=Arr::map($months,function($month){
+            return $month->format('%B');
+        });
+
+        return $months;
+    }
+    
+
+    /**
+     * Getting the days of the week
+     *
+     * @return array
+     */
+    public function daysOfWeek() 
+    {
+        $jalalian =$this->now()->getFirstDayOfWeek();
+        $weekdays=[];
+
+        for ($i=0; $i < 7; $i++) { 
+            $weekdays[] = ($i == 0) ? $jalalian
+                                    : $jalalian->subDays($i);
+        }
+        
+        $weekdays=array_reverse(Arr::map($weekdays,function($day){
+            return str_replace("\u{200C}", ' ', $day->format('%A'));
+        }));
+
+        array_unshift($weekdays,array_pop($weekdays));
+
+        return $weekdays;
+    }
+
+
+    /**
+     * Getting the abbreviation for the days of the week
+     *
+     * @return array
+     */
+    public function daysOfWeekAbbreviations() :array
+    {
+        $week=Arr::map($this->daysOfWeek(),function($day){ 
+            return mb_substr($day,0,1);
+        });
+        
+        return $week;
+    }
+       
+
+    /**
+     * Getting the day number of the month
+     *
+     * @return int
+     */
+    public function dayOfMonth() :int
+    {
+        return $this->now()
+                    ->getDay();
+    }
+
+    
+    /**
+     * It indicates which day of the first week of the month the first day of the month falls on.
+     *
+     * @return int
+     */
+    public function firstDayOfMonthWeekday() :int
+    {
+       return $this->now()
+                   ->getFirstDayOfMonth()
+                   ->getDayOfWeek();
+       
+    }
+
+        
+    /**
+     * It indicates which day of the week the first day of the following month falls on.
+     *
+     * @return int
+     */
+    public function nextMonthFirstDay() :int
+    {
+        return $this->now()
+                    ->addMonths()
+                    ->getFirstDayOfMonth()
+                    ->getDayOfWeek();
+    }
+    
+
+    /**
+     * It returns the last days of the previous month.
+     *
+     * @return self
+     */
+    public function lastDaysPreviousMonth() :array
+    {
+        $days=[];
+        $endOfMonth=$this->now()
+                         ->subMonths()
+                         ->getEndDayOfMonth();
+
+        for ($i=0 ; $i < $this->firstDayOfMonthWeekday() ; $i++) { 
+             $days[]=$endOfMonth->subDays($i)->getDay();
+        }
+
+        $days = array_reverse($days);
+
+        return $days;
+    }
+
+
+    /**
+     * Returns the days of the current month.
+     *
+     * @return array
+     */
+    public function daysInMonth() :array
+    {
+        $days=[];
+        $countOfDaysInMonth=$this->now()
+                                 ->getMonthDays();
+
+        for ($i=1; $i <= $countOfDaysInMonth ; $i++) { 
+            $days[]=$i;
+        }
+        
+        return $days;
+    }
+    
+    
+    /**
+     * It returns the first days of the next month.
+     *
+     * @return array
+     */
+    public function firstDaysNextMonth() :array
+    {
+        $firstDay= $this->now()->addMonths()->getFirstDayOfMonth()->getDay();
+        $days=[];
+        for ($i= $firstDay ; $i < $this->nextMonthFirstDay() ; $i++) { 
+            $days[]=$i;
+        }
+
+        return $days;
+    }
+    
+
+    /**
+     * It forms the calendar grid.
+     *
+     * @return void
+     */
+    public function calenderGridLayout()
+    {
+        $grid= [
+            'last-month' => $this->lastDaysPreviousMonth(),
+            'current-month' => $this->daysInMonth(),
+            'next-month' => $this->firstDaysNextMonth(),
+        ];
+            
+        return $grid;
+    }
+    
+}
