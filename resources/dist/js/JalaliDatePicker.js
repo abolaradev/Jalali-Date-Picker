6ca793c69206baec4a5268d5dfaf2d12cd33d60b
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-09-06 01:08:41 +0330

    feat: add calendar logic to JavaScript

--- resources/dist/js/JalaliDatePicker.js
+++ resources/dist/js/JalaliDatePicker.js
@@ -1,67 +1,136 @@
-document.addEventListener('alpine:init',function(){
-     Alpine.data('jalaliDatePicker',()=>({
-        showCalendar : false,
-       
-        getDayFromDateString() {
-            var date = this.$el.value
-            return parseInt(date.split('/').pop());
+Alpine.data('jalaliDatePicker',()=>({
+    // Controls the visibility of the calendar.
+    showCalendar : false,
+
+    // Stores the currently selected Jalali date.
+    selectedDate : '',
+
+    // Stores today's Jalali date.
+    today : '',
+    
+    // Initializes the DatePicker state and synchronizes it with Livewire.
+    init(){
+        this.$wire.selectedDate = this.selectedDate
+        this.today = this.$wire.get('today')
+    },
+
+    // Extracts the day number from a Jalali date string.
+    getDayFromDateString() {
+        let date = this.$el.value
+        return parseInt(date.split('/').pop());
+    },
+
+
+    // Defines the bindings and behavior of the DatePicker input.
+    input:{
+        type : 'text',
+        readonly : true ,
+        'x-ref' : 'picker',
+        'x-model' : 'selectedDate',
+
+        ['x-on:click'](){
+            this.showCalendar = !this.showCalendar
+        }
+    },
+
+    // Defines the reset button behavior and its transition animations.
+    resetDateButton:{
+        'x-transition:enter': 'transition ease-out duration-200',
+        'x-transition:enter-start': 'opacity-0 translate-x-2',
+        'x-transition:enter-end': 'opacity-100 translate-x-0',
+        'x-transition:leave': 'transition ease-in duration-150',
+        'x-transition:leave-start': 'opacity-100 translate-x-0',
+        'x-transition:leave-end': 'opacity-0 translate-x-2',
+
+        ['x-show'](){
+            return this.selectedDate != ""
         },
 
-
-        input:{
-            type : 'text',
-            readonly : true ,
-            'x-ref' : 'picker',
-           
-
-            ['x-on:click'](){
-                this.showCalendar = !this.showCalendar
-            }
-        },
-
-        calendar:{
-            dir : 'rtl',
-            
-            'x-transition:enter'       : 'transition ease-out duration-200',
-            'x-transition:enter-start' : 'opacity-0 -translate-y-2',
-            'x-transition:enter-end'   : 'opacity-100 translate-y-0',
-            'x-transition:leave'       : 'transition ease-in duration-150',
-            'x-transition:leave-start' : 'opacity-100 translate-y-0',
-            'x-transition:leave-end'   : 'opacity-0 -translate-y-1',
-
-            ['x-show'](){
-                return this.showCalendar
-            },
-
-            ['x-anchor'](){
-                return this.$refs.picker
-            },
-
-            ['x-on:click.outside'](){
-                this.showCalendar = false
-            }
-        },
-
-        unselectableDays:{
-            type : 'button',
-            disabled : true,
-            
-            ['x-text'](){
-                return this.getDayFromDateString();
-            },
-        },
-
-        selectableDays : {
-            type : 'button',
-
-            ['x-text'](){
-                return this.getDayFromDateString();
-            },
-
-            ['x-on:click'](){
-                this.$wire.selectedDate =this.$el.value 
-                this.showCalendar =false;
+        ['x-on:click'](){
+            if(this.selectedDate != "") {
+                this.selectedDate = ""
             }
         }
-    }))
-})
+    },
+
+    // Defines the calendar container behavior, positioning, and transitions.
+    calendar:{
+        dir : 'rtl',
+        
+        'x-transition:enter'       : 'transition ease-out duration-200',
+        'x-transition:enter-start' : 'opacity-0 -translate-y-2',
+        'x-transition:enter-end'   : 'opacity-100 translate-y-0',
+        'x-transition:leave'       : 'transition ease-in duration-150',
+        'x-transition:leave-start' : 'opacity-100 translate-y-0',
+        'x-transition:leave-end'   : 'opacity-0 -translate-y-1',
+
+        ['x-show'](){
+            return this.showCalendar
+        },
+
+        ['x-anchor'](){
+            return this.$refs.picker
+        },
+
+        ['x-on:click.outside'](){
+            if(this.$wire.outsideClose){
+                this.showCalendar = false
+            }
+        }
+    },
+
+    // Defines the bindings for days that cannot be selected.
+    unselectableDays:{
+        type : 'button',
+        disabled : true,
+        
+        ['x-text'](){
+            return this.getDayFromDateString();
+        },
+    },
+
+    // Defines the bindings and behavior for selectable calendar days.
+    selectableDays : {
+        type : 'button',
+        
+        ['x-text'](){
+            return this.getDayFromDateString();
+        },
+
+        ['x-on:click'](){
+            this.selectedDate =this.$el.value 
+
+            if(this.$wire.get('autoClose')){
+                this.showCalendar =false;
+            }
+        },
+
+        // Applies the appropriate styles to the day based on today's
+        // date and the currently selected date.
+        [':class'](){ 
+           const classes = []
+
+           if (this.today == this.$el.value) {
+                classes.push('border')
+           }
+
+           if (this.selectedDate == this.$el.value) {
+                classes.push(
+                    'bg-fuchsia-700',
+                    'text-fuchsia-100',
+                    'font-semibold',
+                    'transition-colors',
+                    'duration-300'
+                )
+           } else {
+                classes.push(
+                    'bg-fuchsia-200',
+                    'text-fuchsia-700',
+                    'hover:bg-fuchsia-300'
+                )
+           }
+
+            return classes.join(' ')
+        },
+    }
+}))
