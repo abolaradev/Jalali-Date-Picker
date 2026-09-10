1ff524e2aa84331cbf9fa4da8d384edd684edc6b
Abolfazl Ebrahimi <abolfazlebrahimi923395@gmail.com>
2026-09-10 19:40:20 +0330

    fix: fix package asset loading

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -4,8 +4,9 @@ use Abolaradev\JalaliDatePicker\Facades\JalaliDatePicker;
 
 function package_asset(string $path)
 {
-    $asset = asset("vendor/jalali-date-picker/$path");
-    $path = file_exists($asset) ? $asset
-                                : url()->query("assets/$path" ,['ver'=>time()]);
-    return $path;
+   $filePath = public_path("vendor/jalali-date-picker/$path");
+
+    return file_exists($filePath)
+           ? asset("vendor/jalali-date-picker/$path")
+           : url()->query("assets/$path" ,['ver'=>time()]);
 }
